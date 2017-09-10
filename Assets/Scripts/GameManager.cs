f5271c3bebe9dd5ff8f7c8f103cf3cafec92371c
Julien De Conti <julien.de.conti@gmail.com>
2017-09-10 20:19:20 +0200

    [LevelDesign][Tank] Add the first mechanisms of tank movement

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,18 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+namespace Diep3D
 {
-
-    // Use this for initialization
-    void Start()
+    public class GameManager : MonoBehaviour
     {
 
-    }
+        // Use this for initialization
+        void Start()
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
+        }
 
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
