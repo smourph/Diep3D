b06a38f0ee30cdf27c0e414051543a3d73b326b2
Julien De Conti <julien.de.conti@gmail.com>
2017-09-11 00:06:57 +0200

    [LevelDesign][Tank] Init turrent and gun behaviours when camera moves

---
+++ Assets/Scripts/Tank/TurretMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace Diep3D.Tank
+{
+	public class TurretMovement : MonoBehaviour
+	{
+        private Transform m_followingCamera;
+
+		private void Awake()
+		{
+			// Get the transform of the main camera
+			if (Camera.main != null)
+			{
+				m_followingCamera = Camera.main.transform;
+			}
+			else
+			{
+				Debug.LogWarning("Warning: no main camera found. Tank needs a Camera tagged \"MainCamera\", for camera-relative controls.");
+			}
+		}
+
+		private void Update()
+		{
+		}
+         
+
+		private void FixedUpdate()
+		{
+			transform.eulerAngles = new Vector3(transform.eulerAngles.x, m_followingCamera.eulerAngles.y, transform.eulerAngles.z);
+		}
+	}
+}
