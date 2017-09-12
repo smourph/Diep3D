34b8f8baccd6d65e7f47019d3128a2b85600dbdc
Julien De Conti <julien.de.conti@gmail.com>
2017-09-12 21:17:57 +0200

    [LevelDesign][Tank] Turret can aim up and down farther (-15° to 75°)

--- Assets/Scripts/Tank/Gun.cs
+++ Assets/Scripts/Tank/Gun.cs
@@ -26,18 +26,18 @@ namespace Diep3D.Tank
 
         private void FixedUpdate()
         {
-            // The gun follow the ascent and descent of the aim (betwen 0° and 45° up)
-            if (m_followingCamera.eulerAngles.x < 270f) // camera look at bottom or behind (shall it be possible ?)
+            // The gun follow the ascent and descent of the aim (betwen -15° and 30° up)
+            if (m_followingCamera.eulerAngles.x <= 5f || m_followingCamera.eulerAngles.x >= 285f) // -15 to 75°
+			{
+				transform.eulerAngles = new Vector3(m_followingCamera.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z);
+			}
+			else if (m_followingCamera.eulerAngles.x < 270f) // -90° to -15
             {
-                transform.eulerAngles = new Vector3(0f, transform.eulerAngles.y, transform.eulerAngles.z);
+                transform.eulerAngles = new Vector3(5f, transform.eulerAngles.y, transform.eulerAngles.z);
             }
-            else if (m_followingCamera.eulerAngles.x <= 315f) // 45° to 90°
-            {
-                transform.eulerAngles = new Vector3(315f, transform.eulerAngles.y, transform.eulerAngles.z);
-            }
-            else if (m_followingCamera.eulerAngles.x > 315f) // 0 to 45°
-            {
-                transform.eulerAngles = new Vector3(m_followingCamera.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z);
+            else if (m_followingCamera.eulerAngles.x < 285f) // 75° to 90°
+			{
+                transform.eulerAngles = new Vector3(285f, transform.eulerAngles.y, transform.eulerAngles.z);
             }
         }
     }
