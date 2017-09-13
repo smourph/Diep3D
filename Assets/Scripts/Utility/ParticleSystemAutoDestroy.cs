cb331b6b650e0570f20e3a4659ed21eab0364358
Smourph <julien.de.conti@gmail.com>
2017-09-13 13:41:56 +0200

    [GameDesign][Bullet] Create a script that autodestroy a multiple particle systems and move Explosion to custom prefabs folder

---
+++ Assets/Scripts/Utility/ParticleSystemAutoDestroy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Diep3D.Utility
+{
+    public class ParticleSystemAutoDestroy : MonoBehaviour
+    {
+        private float m_MaxLifetime;
+        private bool m_EarlyStop;
+
+        private IEnumerator Start()
+        {
+            var systems = GetComponentsInChildren<ParticleSystem>();
+
+            // find out the maximum lifetime of any particles in this effect
+            foreach (var system in systems)
+            {
+                m_MaxLifetime = Mathf.Max(system.main.startLifetime.constant, m_MaxLifetime);
+            }
+
+            // wait for any remaining particles to expire
+            yield return new WaitForSeconds(m_MaxLifetime);
+
+            Destroy(gameObject);
+        }
+
+
+        public void Stop()
+        {
+            // stops the particle system early
+            m_EarlyStop = true;
+        }
+    }
+}
